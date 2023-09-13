5905479744e10605f0bf8b38732eda9144c04324
vladkick <vladkick2014@gmail.com>
2023-09-13 14:07:24 +0300

    name1

---
+++ src/HelloWorldDemo.java
@@ -0,0 +1,6 @@
+public class HelloWorldDemo {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+        System.out.println("This task done by Vladysla Syzov from PZ-20-1/9");
+    }
+}
