cd34bf7dc6b5da17d28ea161aaaa840064fa8300
vladkick <vladkick2014@gmail.com>
2023-09-14 02:04:44 +0300

    name6

--- src/HelloWorldDemo.java
+++ src/HelloWorldDemo.java
@@ -1,6 +1,13 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class HelloWorldDemo {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("This task was done by Vladyslav Syzov from PZ-20-1/9 ");
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String formattedDate = currentDate.format(formatter);
+        System.out.println("Поточна дата: " + formattedDate);
     }
 }
